7d082ebc4b29c49a992742f2e4a11328dad9048d
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-30 17:19:26 +0100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 import setuptools
 
-with open("README.md", 'r') as fh:
-    long_description = fh.read()
+def get_long_description():
+    with open("README.md", 'r') as fh:
+        long_description = fh.read()
+    return long_description
+
+def get_requirements():
+    with open("requirements", 'r') as fh:
+        requirements = fh.read().splitlines()
+    return requirements
 
 setuptools.setup(
     name="atsge",
-    version="0.1.2",
+    version="0.1.3",
     author="Shane Breeze",
     author_email="sdb15@ic.ac.uk",
     scripts=[],
     description="SGE batch job submission with AlphaTwirl",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/shane-breeze/atsge",
     packages=setuptools.find_packages(),
-    download_url="https://github.com/shane-breeze/atsge/archive/0.1.2.tar.gz",
+    download_url="https://github.com/shane-breeze/atsge/archive/0.1.3.tar.gz",
+    install_requires=get_requirements(),
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     classifiers=[
