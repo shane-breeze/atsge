e26913e2752f7faf5fdf070b31a21e6220ac84a2
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-30 17:21:44 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements():
 
 setuptools.setup(
     name="atsge",
-    version="0.1.3",
+    version="0.1.4",
     author="Shane Breeze",
     author_email="sdb15@ic.ac.uk",
     scripts=[],
@@ -21,7 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/shane-breeze/atsge",
     packages=setuptools.find_packages(),
-    download_url="https://github.com/shane-breeze/atsge/archive/0.1.3.tar.gz",
+    download_url="https://github.com/shane-breeze/atsge/archive/0.1.4.tar.gz",
     install_requires=get_requirements(),
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
